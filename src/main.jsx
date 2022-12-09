60c168064e27a6bd0404f95d0bad1f044d380028
Gabriel S. Koller <95451205+GABRIEL-KOLLER@users.noreply.github.com>
2022-12-09 20:29:54 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.css";
+
+import { Home } from "./pages/Home/Index.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+import { Card } from "../../components/Card";
+
+export function Home() {
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: "", avatar: "" });
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/GABRIEL-KOLLER")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        });
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Programming students</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Photo" />
+        </div>
+      </header>
+      <input
+        type="text"
+        placeholder="Writer your name..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+      <button type="button" onClick={handleAddStudent}>
+        Add
+      </button>
+
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
+    </div>
+  );
+}
